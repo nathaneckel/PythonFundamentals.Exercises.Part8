3cb639fcc6e8c2082825274af4f9fd9e2c14cc58
nathaneckel <nathan@patientpaperwork.com>
2024-03-19 20:57:22 -0400

    getting closer

--- tree.py
+++ tree.py
@@ -3,20 +3,28 @@ from os import listdir
 from os.path import isfile
 def tree():
 
-    str.path = os.walk("/Users/nathan/Dev/Labs/PythonFundamentals.Exercises.Part8/Exercise2")
-# path = os.walk("~/Dev/labs/Exercise2")
+    #str.path = os.walk("/Users/nathan/Dev/Labs/PythonFundamentals.Exercises.Part8/Exercise2")
+    str.path = os.walk("~/Dev/labs/Exercise2")
 
 
 for f in listdir(str.path):
     if not isfile(str.path):
-        filepath = "{0:s}/dummy.txt".format(str.path)
+        filepath = "{0:s}/~/Dev/labs/Exercise2".format(str.path)
         with open(filepath, 'w') as f:
             f.write('Hi there!')
+tree()
 
-            # file.filename = "myfile.txt"
-            #     file.filetext = "mytext"
-            # #directories = os.walk("~/Dev/labs/Exercise2")
-            # directories = os.walk("/Users/nathan/Dev/Labs/PythonFundamentals.Exercises.Part8/Exercise2")
-            # for directory in directories:
-            #     with open(directory[0]+"\\"+filename, "w") as file:
-            #         file.write(filetext)
+
+
+
+
+
+#     file.filename = "*"
+#     file.filetext = "*"
+#             #directories = os.walk("~/Dev/labs/Exercise2")
+# directories = os.walk("/Users/nathan/Dev/Labs/PythonFundamentals.Exercises.Part8/Exercise2")
+#     for directory in directories:
+#                 with open(directory[0]+"\\"+filename, "w") as file:
+#                     file.write(filetext)
+#
+# tree()
